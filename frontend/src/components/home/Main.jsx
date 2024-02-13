2d89d4208265aebeefd66e7e355542a176242a1d
Jahz10 <bullymedont.justify@gmail.com>
2024-02-13 22:21:39 +0800

    update home page

--- frontend/src/components/home/Main.jsx
+++ frontend/src/components/home/Main.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import CategoryModal from "./CategoryModal";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import "./Main.css";
 
@@ -83,20 +85,23 @@ const Main = () => {
       <div className="overlay h-full w-full">
         <div className="relative px-4 h-full w-full flex flex-col justify-center text-white font-bold">
           {/* Main Text */}
-          <div className="grid place-items-center gap-2 lg:gap-4 text-center">
-            <h1 className="text-3xl font-black sm:text-4xl lg:text-5xl text">
-              Book, Create, Celebrate
-            </h1>
-            <h1 className="text-lg sm:text-2xl lg:text-5xl font-black">
-              Unleash the Magic of our Spaces
-            </h1>
-            {/* Main button  */}
-            <button
-              onClick={() => setIsOpenModal(true)}
-              className="py-3 px-6 rounded-xl mt-4 bg-yellow-300/90 font-bold shadow-xl"
-            >
-              Check Availability
-            </button>
+          <div className="relative px-4 h-full w-full flex flex-col gap-8 items-center justify-center text-white font-bold">
+            {/* Main Text */}
+            <div className="grid place-items-center gap-2 lg:gap-4 text-center">
+              <h1 className="text-3xl font-black sm:text-4xl lg:text-5xl text">
+                Book, Create, Celebrate
+              </h1>
+              <h1 className="text-lg sm:text-2xl lg:text-5xl font-black">
+                Unleash the Magic of our Spaces
+              </h1>
+              {/* Main button  */}
+              <button
+                onClick={() => setIsOpenModal(true)}
+                className="py-3 px-6 rounded-xl mt-4 bg-yellow-300/90 font-bold shadow-xl"
+              >
+                Check Availability
+              </button>
+            </div>
           </div>
         </div>
         {/* <BsChevronCompactLeft
@@ -122,11 +127,16 @@ const Main = () => {
           />
         ))}
       </div> */}
+      <div className="absolute bottom-0 h-20 w-full flex justify-center items-center bg-blue-900 blur" />
       <div
-        className={`modal-background ${openModal ? "visible" : "hidden"}`}
         onClick={() => setIsOpenModal(false)}
+        className={
+          openModal ? ` fixed w-full h-screen bg-black/70 top-0 left-0` : ""
+        }
       />
-      {openModal && <CategoryModal setIsOpenModal={setIsOpenModal} />}
+      {openModal && (
+        <CategoryModal openModal={openModal} setIsOpenModal={setIsOpenModal} />
+      )}
     </div>
   );
 };
@@ -208,24 +218,24 @@ export default Main;
 //       }")] bg-no-repeat bg-cover bg-center`}
 //     >
 //       <div className="absolute top-0 left-0 h-full w-full bg-blue-500/50" />
-//       <div className="relative px-4 h-full w-full flex flex-col gap-8 items-center justify-center text-white font-bold">
-//         {/* Main Text */}
-//         <div className="grid place-items-center gap-2 lg:gap-4 text-center">
-//           <h1 className="text-3xl font-black sm:text-4xl lg:text-5xl text">
-//             Book, Create, Celebrate
-//           </h1>
-//           <h1 className="text-lg sm:text-2xl lg:text-5xl font-black">
-//             Unleash the Magic of our Spaces
-//           </h1>
-//           {/* Main button  */}
-//           <button
-//             onClick={() => setIsOpenModal(true)}
-//             className="py-3 px-6 rounded-xl mt-4 bg-yellow-300/90 font-bold shadow-xl"
-//           >
-//             Check Availability
-//           </button>
-//         </div>
-//       </div>
+// <div className="relative px-4 h-full w-full flex flex-col gap-8 items-center justify-center text-white font-bold">
+//   {/* Main Text */}
+//   <div className="grid place-items-center gap-2 lg:gap-4 text-center">
+//     <h1 className="text-3xl font-black sm:text-4xl lg:text-5xl text">
+//       Book, Create, Celebrate
+//     </h1>
+//     <h1 className="text-lg sm:text-2xl lg:text-5xl font-black">
+//       Unleash the Magic of our Spaces
+//     </h1>
+//     {/* Main button  */}
+//     <button
+//       onClick={() => setIsOpenModal(true)}
+//       className="py-3 px-6 rounded-xl mt-4 bg-yellow-300/90 font-bold shadow-xl"
+//     >
+//       Check Availability
+//     </button>
+//   </div>
+// </div>
 
 //       <BsChevronCompactLeft
 //         size={30}
@@ -250,16 +260,16 @@ export default Main;
 //           />
 //         ))}
 //       </div>
-//       <div className="absolute bottom-0 h-20 w-full flex justify-center items-center bg-blue-900 blur" />
-//       <div
-//         onClick={() => setIsOpenModal(false)}
-//         className={
-//           openModal ? ` fixed w-full h-screen bg-black/70 top-0 left-0` : ""
-//         }
-//       />
-//       {openModal && (
-//         <CategoryModal openModal={openModal} setIsOpenModal={setIsOpenModal} />
-//       )}
+// <div className="absolute bottom-0 h-20 w-full flex justify-center items-center bg-blue-900 blur" />
+// <div
+//   onClick={() => setIsOpenModal(false)}
+//   className={
+//     openModal ? ` fixed w-full h-screen bg-black/70 top-0 left-0` : ""
+//   }
+// />
+// {openModal && (
+//   <CategoryModal openModal={openModal} setIsOpenModal={setIsOpenModal} />
+// )}
 //     </div>
 //   );
 // };
